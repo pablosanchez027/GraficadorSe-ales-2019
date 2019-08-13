658c2af1bf1e66f82cc55e131afc610b6c114812
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-13 11:47:46 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorDeSeñales/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraficadorDeSeñales
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            plnGrafica.Points.Add(new Point(0, 10));
+            plnGrafica.Points.Add(new Point(20, 15));
+            plnGrafica.Points.Add(new Point(100, 50));
+            plnGrafica.Points.Add(new Point(200, 1));
+            plnGrafica.Points.Add(new Point(300, 70));
+            plnGrafica.Points.Add(new Point(1000, 70));
+        }
+
+        private void BtnGraficar_Click(object sender, RoutedEventArgs e)
+        {
+            double amplitud = double.Parse(txtAmplitud.Text);
+            double fase = double.Parse(txtFase.Text);
+            double frecuencia = double.Parse(txtFrecuencia.Text);
+            double tiempoinicial = double.Parse(txtTiempoInicial.Text);
+            double tiempofinal = double.Parse(txtTiempoFinal.Text);
+            double frecuenciamuestreo = double.Parse(txtFrecuenciaMuestreo.Text);
+
+            SeñalSenoidal señal = new SeñalSenoidal(amplitud, fase, frecuencia);
+
+            double periodioMuestreo = 1.0 / frecuenciamuestreo;
+
+            plnGrafica.Points.Clear();
+            for(double i = tiempoinicial; i <= tiempofinal; i += periodioMuestreo)
+            {
+
+            }
+        }
+    }
+}
