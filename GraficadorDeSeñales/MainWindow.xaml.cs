f08d103780c5e2da7326d41ca5b78fe5316ce4bb
Pablo Sánchez <pablosanchez027@hotmail.com>
2019-10-22 10:48:38 -0700

    Transformada e Indice Máximo

--- GraficadorDeSeñales/MainWindow.xaml.cs
+++ GraficadorDeSeñales/MainWindow.xaml.cs
@@ -245,6 +245,20 @@ namespace GraficadorDeSeñales
                     );
             }
 
+            if (cbOperacion.SelectedIndex == 4)
+            {
+                int indiceMaximo = 0;
+                for(int i=0;i<señalResultante.Muestras.Count/2;i++)
+                {
+                    if (señalResultante.Muestras[i].Y > señalResultante.Muestras[indiceMaximo].Y)
+                    {
+                        indiceMaximo = i;
+                    }
+                    double frecuencia = (double)(indiceMaximo * señalResultante.FrecuenciaMuestreo)
+                       / (double)señalResultante.Muestras.Count;
+                    lblHertz.Text = frecuencia.ToString();
+                }
+            }
 
             lblLimiteSuperior.Text =
                 amplitudMaxima.ToString("F");
